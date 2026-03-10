c17dd909cfce4b7da075066c112cbe287b0fc4f7
tenshicakes <jmontalan8@gmail.com>
2026-03-10 12:13:48 +0800

    minor comment changes

--- mainfile.js
+++ mainfile.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-// convenient route to serve register page
+// route to serve register page
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
